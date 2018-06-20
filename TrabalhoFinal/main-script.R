99bbab3f8e9e3ae8ed8710a717ef4931ebd2c0af
Cezar Pinheiro Schroeder <cezar.schroeder@gmail.com>
2018-06-20 18:45:19 -0300

    Concluída a importação da base de dados e a exibição de sua estrutura.

---
+++ TrabalhoFinal/main-script.R
@@ -0,0 +1,14 @@
+# Base de Dados Retirada de:
+# https://archive.ics.uci.edu/ml/datasets/statlog+(german+credit+data)
+#
+# Importação da Base de Dados
+credit_data <- read.csv("credit.csv")
+
+# Exploração, Preparação e Pré-Processamento dos Dados
+
+# Exibição da Estrutura dos Dados (Atributos e Rótulo)
+str(credit_data)
+
+# Estatística Exploratória Para os Atributos Categóricos
+
+# Estatística Exploratória Para os Atributos Numéricos
