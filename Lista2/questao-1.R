2112a111c4eee6c8d2bf5e5717a0ddf44f43fdd8
Cezar Pinheiro Schroeder <cezar.schroeder@gmail.com>
2018-06-28 15:44:32 -0300

    Concluída a resolução da questão 1.

--- Lista2/questao-1.R
+++ Lista2/questao-1.R
@@ -1,1 +1,19 @@
 # Script Principal para a Questão 1
+
+# Pacotes Utilizados
+library(nnet) # Modelo MLP
+library(caret) # Avaliação de Desempenho
+
+# Base de Dados Retirada de:
+# http://archive.ics.uci.edu/ml/datasets/Iris
+#
+# Importação da Base de Dados
+iris_data <- read.csv("iris_data.csv")
+
+# Configuração e Projeto do Experimento de Avaliação 
+
+# Utilizando Validação Cruzada em 10 Partes Para Treinamento e Validação do Modelo MLP
+training_ctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 10)
+
+# Treinamento e Validação (Ajuste de Parâmetros) para o Modelo
+mlp_model_eval <- train(class ~ ., data = iris_data, method = "nnet", metric = "Accuracy", trControl = training_ctrl)
